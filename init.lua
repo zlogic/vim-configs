3aa79d755e946cdbbd403d6393e6eb47e4ced9c9
Dmitrii Zolotukhin <dmitrii.zolothukhin@mendix.com>
2024-02-28 14:27:14 +0100

    Do not check unused params.
    Some callbacks use empty params and result in false positives.

--- init.lua
+++ init.lua
@@ -9,9 +9,6 @@ local lspconfig = require('lspconfig')
 lspconfig.gopls.setup({
   settings = {
     gopls = {
-      analyses = {
-        unusedparams = true,
-      },
       staticcheck = true,
       gofumpt = true,
     },
