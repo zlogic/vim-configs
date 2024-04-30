67f0fd153ffa497979c334293b6b34e6ce363d99
Dmitry Zolotukhin <zlogic@gmail.com>
2024-04-30 18:04:25 +0200

    Enable smart case search.

--- init.lua
+++ init.lua
@@ -6,12 +6,14 @@ vim.opt.visualbell = true
 
 vim.wo.number = true
 
-vim.o.smartcase = true
+vim.opt.smartcase = true
+vim.opt.ignorecase = true
 
 vim.opt.smartindent = true
 vim.opt.tabstop = 4
 vim.opt.shiftwidth = 4
 -- vim.opt.softtabstop = 4
+vim.opt.expandtab = true
 
 -- vim.cmd.colorscheme('habamax')
 
