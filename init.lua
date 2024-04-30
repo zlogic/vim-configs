ef4bd090e0f8b89ee465ca9edae1f8d029d48f71
Dmitry Zolotukhin <zlogic@gmail.com>
2024-04-30 16:22:58 +0200

    Cleaned up and aligned with Vim config.

--- init.lua
+++ init.lua
@@ -1,5 +1,17 @@
+-- Behave like vim (middle-click paste)
+vim.opt.mouse = ''
+vim.opt.belloff = ''
+-- vim.opt.errorbells = true
+vim.opt.visualbell = true
+
 vim.wo.number = true
+
 vim.o.smartcase = true
+
+vim.opt.smartindent = true
+vim.opt.tabstop = 4
+vim.opt.shiftwidth = 4
+-- vim.opt.softtabstop = 4
 
 -- vim.cmd.colorscheme('habamax')
 
@@ -11,6 +23,9 @@ lspconfig.gopls.setup({
     gopls = {
       staticcheck = true,
       gofumpt = true,
+      -- Changes to advanced syntax highligting
+      -- semanticTokens = true,
+      usePlaceholders = true,
     },
   },
 })
@@ -22,15 +37,22 @@ vim.keymap.set('n', '<space>q', vim.diagnostic.setloclist)
 
 vim.api.nvim_create_autocmd('LspAttach', {
   group = vim.api.nvim_create_augroup('UserLspConfig', {}),
-  callback = function(ev)
+  callback = function(args)
+    local client = vim.lsp.get_client_by_id(args.data.client_id)
     -- Enable completion triggered by <c-x><c-o>
-    vim.bo[ev.buf].omnifunc = 'v:lua.vim.lsp.omnifunc'
+    if client.server_capabilities.completionProvider then
+      vim.bo[args.buf].omnifunc = "v:lua.vim.lsp.omnifunc"
+    end
+    if client.server_capabilities.definitionProvider then
+      vim.bo[args.buf].tagfunc = "v:lua.vim.lsp.tagfunc"
+    end
 
     -- Buffer local mappings.
     -- See `:help vim.lsp.*` for documentation on any of the below functions
-    local opts = { buffer = ev.buf }
+    local opts = { buffer = args.buf }
     vim.keymap.set('n', 'gD', vim.lsp.buf.declaration, opts)
     vim.keymap.set('n', 'gd', vim.lsp.buf.definition, opts)
+    vim.keymap.set('n', '<C-w>gd', vim.lsp.buf.definition, opts)
     vim.keymap.set('n', 'K', vim.lsp.buf.hover, opts)
     vim.keymap.set('n', 'gi', vim.lsp.buf.implementation, opts)
     vim.keymap.set('n', '<C-k>', vim.lsp.buf.signature_help, opts)
@@ -38,20 +60,21 @@ vim.api.nvim_create_autocmd('LspAttach', {
     vim.keymap.set('n', '<space>rn', vim.lsp.buf.rename, opts)
     vim.keymap.set({ 'n', 'v' }, '<space>ca', vim.lsp.buf.code_action, opts)
     vim.keymap.set('n', 'gr', vim.lsp.buf.references, opts)
-    vim.keymap.set('n', '<space>f', function()
-      vim.lsp.buf.format { async = true }
-    end, opts)
+    vim.keymap.set('n', '<space>f', function() vim.lsp.buf.format { async = false } end, opts)
+    vim.keymap.set('n', 'gr', vim.lsp.buf.references, opts)
+    vim.keymap.set('n', '<space>cc', vim.lsp.codelens.run, opts)
+    vim.keymap.set('n', '<space>cC', vim.lsp.codelens.refresh, opts)
   end,
 })
 
 local augroup = vim.api.nvim_create_augroup("LspFormatting", {})
 
 vim.api.nvim_create_autocmd('BufWritePre', {
-  pattern = '*.go',
+  pattern = '*.go,*.rs',
   group = 'LspFormatting',
   callback = function()
     vim.lsp.buf.format({ async = true })
-  end,
+  end
 })
 
 vim.api.nvim_create_autocmd({"BufNewFile","BufRead"}, {
@@ -62,3 +85,6 @@ vim.api.nvim_create_autocmd({"BufNewFile","BufRead"}, {
     vim.opt_local.shiftwidth = 4
   end
 })
+
+-- Autoclose preview window (alternatively, use <C-w>z)
+vim.api.nvim_create_autocmd('CompleteDone', { callback = function() vim.cmd.pclose() end })
